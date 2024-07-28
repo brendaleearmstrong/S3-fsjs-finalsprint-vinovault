d5fb155b0b41eaa86b78a96c241140e91b116f3d
Brenda Armstrong <137068502+brendaleearmstrong@users.noreply.github.com>
2024-07-28 19:43:12 +0200

    Setup project structure and initial files

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
