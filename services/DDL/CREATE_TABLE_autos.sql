8a4eb9df65e6e3086b5290f098001252e81c94c1
Brenda Armstrong <137068502+brendaleearmstrong@users.noreply.github.com>
2024-07-31 17:14:56 +0200

    deleting

--- services/DDL/CREATE_TABLE_autos.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE autos (
-    auto_id SERIAL PRIMARY KEY,
-    make VARCHAR(50),
-    model VARCHAR(50),
-    year INT,
-    vin VARCHAR(17),
-    description VARCHAR(255)
-);
--- services/m.db.js
+++
@@ -1,5 +0,0 @@
-const { MongoClient } = require('mongodb');
-const uri = process.env.MDBLOCAL // "mongodb://127.0.0.1:27017/";
-const pool = new MongoClient(uri);
-
-module.exports = pool;
